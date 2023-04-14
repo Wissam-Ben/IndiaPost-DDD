dc751a0ebede7f9bbbcce6e5617410ac86c3bebc
Paolo Manaois <paolo.manaois@yahoo.fr>
2023-04-14 14:46:38 +0200

    lightweight syntax for records

--- Livraison/model/LotLivraison/Adresse.cs
+++ Livraison/model/LotLivraison/Adresse.cs
@@ -1,13 +1,3 @@
 namespace Livraison.Model.LotLivraisonAggregate;
 
-public class Adresse
-{
-	public string Ville { get; init; }
-	public string Secteur { get; init; }
-
-	public Adresse(string ville, string secteur)
-	{
-		Ville = ville;
-		Secteur = secteur;
-	}
-}
+public record Adresse(string Ville, string Secteur);
--- Livraison/model/LotLivraison/Colis.cs
+++ Livraison/model/LotLivraison/Colis.cs
@@ -1,15 +1,3 @@
 namespace Livraison.Model.LotLivraisonAggregate;
 
-public record Colis
-{
-	public string ProduitID { get; init; }
-	public int Quantite { get; init; }
-	public Adresse Adresse { get; init; }
-
-	public Colis(string produitID, int quantite, Adresse adresse)
-	{
-		ProduitID = produitID;
-		Quantite = quantite;
-		Adresse = adresse;
-	}
-}
+public record Colis(string ProduitID, int Quantite, Adresse Adresse);
